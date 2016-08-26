c606bef7fbd4b5cd520bdfa04d9ef26975e31ac4
hxh <hxh@ike-global.com>
2016-08-26 14:47:09 +0800

    完成微信文本消息功能

---
+++ TAF.Owin/Weixin/Controller/WeixinController.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CLASS.cs" company="" author="何翔华">
+//   
+// </copyright>
+// <summary>
+//   CLASS
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using TAF.Mvc;
+using TAF.Utility;
+
+namespace TAF.Owin.Controller
+{
+    using System.Linq;
+    using System.Net;
+    using System.Web.Http.Controllers;
+
+    using TAF.Owin.Model;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WeixinController : BaseController
+    {
+        [AllowAnonymous]
+        public HttpResponseMessage Get(string signature, string timestamp, string nonce, string echostr)
+        {
+            var result = Validate(this.RequestContext);
+            return new HttpResponseMessage() { Content = new StringContent(echostr, Encoding.GetEncoding("UTF-8"), "application/x-www-form-urlencoded") };
+        }
+
+        [AllowAnonymous]
+        public HttpResponseMessage Post()
+        {
+            if (Validate(this.RequestContext))
+            {
+                var content = this.RequestContext.Url.Request.Content.ReadAsStringAsync().Result;
+                var message = TextMessage.ReceveMessage(content);
+                message.Content = Randoms.GetRandomCode(5);
+                return new HttpResponseMessage() { Content = new StringContent(message.SendMessage(), Encoding.GetEncoding("UTF-8"), "application/x-www-form-urlencoded") };
+            }
+
+            return new HttpResponseMessage() { StatusCode = HttpStatusCode.Forbidden };
+        }
+
+        private static bool Validate(HttpRequestContext context)
+        {
+            var query = context.Url.Request.GetQueryNameValuePairs();
+            var signature = query.Single(r => r.Key == "signature").Value;
+            var timestamp = query.Single(r => r.Key == "timestamp").Value;
+            var nonce = query.Single(r => r.Key == "nonce").Value;
+            var token = ConfigManager.Instance.GetConfig<string>("Weixin", "Token");
+            var array = new string[] { token, timestamp, nonce };
+            Array.Sort(array);
+            var newStr = $"{array[0]}{array[1]}{array[2]}";
+            var result = Encrypt.SHA1(newStr);
+            LogManager.Instance.Logger.Info($"result:{result}");
+            LogManager.Instance.Logger.Info($"signature:{signature}");
+            LogManager.Instance.Logger.Info($"timestamp:{timestamp}");
+            LogManager.Instance.Logger.Info($"nonce:{nonce}");
+            return result == signature;
+        }
+    }
+
+
+}
---
+++ TAF.Owin/Weixin/Model/TextMessage.cs
@@ -0,0 +1,92 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TextMessage.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   文本消息
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TAF.Owin.Model
+{
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// 文本消息
+    /// </summary>
+    public class TextMessage
+    {
+        /// <summary>
+        /// 消息接收者
+        /// </summary>
+        public string ToUserName
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 消息发出者
+        /// </summary>
+        public string FromUserName
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 创建事件
+        /// </summary>
+        public string CreateTime
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 消息内容
+        /// </summary>
+        public string Content
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        public string MsgId
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 收到微信推送消息
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static TextMessage ReceveMessage(string content)
+        {
+            var reg0 = new Regex(@"\[CDATA\[(.*?)\]\]");
+            var reg1 = new Regex(@"<(.*?)>(\d+)<\/(.*?)>");
+            var matchs0 = reg0.Matches(content);
+            var matchs1 = reg1.Matches(content);
+            var fromMessage = new TextMessage
+            {
+                ToUserName = matchs0[0].Groups[1].Value,
+                FromUserName = matchs0[1].Groups[1].Value,
+                Content = matchs0[3].Groups[1].Value,
+                CreateTime = matchs1[0].Groups[2].Value,
+                MsgId = matchs1[1].Groups[2].Value,
+            };
+            return fromMessage;
+        }
+
+        public  string SendMessage()
+        {
+            var toMessage = $@"<xml><ToUserName><![CDATA[{ FromUserName}]]></ToUserName>
+                <FromUserName><![CDATA[{ToUserName}]]></FromUserName>
+                <CreateTime>{CreateTime}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[{Content}]]></Content>
+                </xml>";
+            return toMessage;
+        }
+    }
+}
