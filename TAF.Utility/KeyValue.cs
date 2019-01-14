2c1fe635ad624d5a790ad8f88be5f5ce5c08f7ad
he <thornfield_he@126.com>
2019-01-14 11:04:23 +0800

    KeyValue对象允许序列化

--- TAF.Utility/KeyValue.cs
+++ TAF.Utility/KeyValue.cs
@@ -7,6 +7,9 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+using System.Runtime.Serialization;
+
 namespace TAF.Utility
 {
     /// <summary>
@@ -16,6 +19,8 @@ namespace TAF.Utility
     /// </typeparam>
     /// <typeparam name="K2">
     /// </typeparam>
+    [DataContract]
+    [Serializable]
     public class KeyValue<K1, K2>
     {
         /// <summary>
@@ -30,23 +35,17 @@ namespace TAF.Utility
         /// <param name="item2"></param>
         public KeyValue(K1 item1, K2 item2)
         {
-            this.Key = item1;
+            this.Key   = item1;
             this.Value = item2;
         }
 
         /// <summary>
         /// </summary>
-        public K1 Key
-        {
-            get; set;
-        }
+        public K1 Key { get; set; }
 
         /// <summary>
         /// </summary>
-        public K2 Value
-        {
-            get; set;
-        }
+        public K2 Value { get; set; }
     }
 
     /// <summary>
@@ -55,6 +54,8 @@ namespace TAF.Utility
     /// <typeparam name="K1"></typeparam>
     /// <typeparam name="K2"></typeparam>
     /// <typeparam name="K3"></typeparam>
+    [DataContract]
+    [Serializable]
     public class KeyValue<K1, K2, K3> : KeyValue<K1, K2>
     {
         public KeyValue()
@@ -62,12 +63,12 @@ namespace TAF.Utility
         }
 
         public KeyValue(K1 item1, K2 item2, K3 item3)
-            : base(item1, item2) { this.Item3 = item3; }
-
-        public K3 Item3
+            : base(item1, item2)
         {
-            get; set;
+            this.Item3 = item3;
         }
+
+        public K3 Item3 { get; set; }
     }
 
     /// <summary>
@@ -81,6 +82,8 @@ namespace TAF.Utility
     /// </typeparam>
     /// <typeparam name="K4">
     /// </typeparam>
+    [DataContract]
+    [Serializable]
     public class KeyValue<K1, K2, K3, K4> : KeyValue<K1, K2, K3>
     {
         /// <summary>
@@ -90,12 +93,12 @@ namespace TAF.Utility
         }
 
         public KeyValue(K1 item1, K2 item2, K3 item3, K4 item4)
-            : base(item1, item2, item3) { this.Item4 = item4; }
-
-        public K4 Item4
+            : base(item1, item2, item3)
         {
-            get; set;
+            this.Item4 = item4;
         }
+
+        public K4 Item4 { get; set; }
     }
 
     /// <summary>
@@ -110,6 +113,8 @@ namespace TAF.Utility
     /// <typeparam name="K4">
     /// </typeparam>
     /// <typeparam name="K5"></typeparam>
+    [DataContract]
+    [Serializable]
     public class KeyValue<K1, K2, K3, K4, K5> : KeyValue<K1, K2, K3, K4>
     {
         /// <summary>
@@ -119,12 +124,12 @@ namespace TAF.Utility
         }
 
         public KeyValue(K1 item1, K2 item2, K3 item3, K4 item4, K5 item5)
-            : base(item1, item2, item3, item4) { this.Item5 = item5; }
-
-        public K5 Item5
+            : base(item1, item2, item3, item4)
         {
-            get; set;
+            this.Item5 = item5;
         }
+
+        public K5 Item5 { get; set; }
     }
 
     /// <summary>
@@ -140,6 +145,8 @@ namespace TAF.Utility
     /// </typeparam>
     /// <typeparam name="K5"></typeparam>
     /// <typeparam name="K6"></typeparam>
+    [DataContract]
+    [Serializable]
     public class KeyValue<K1, K2, K3, K4, K5, K6> : KeyValue<K1, K2, K3, K4, K5>
     {
         /// <summary>
@@ -149,12 +156,12 @@ namespace TAF.Utility
         }
 
         public KeyValue(K1 item1, K2 item2, K3 item3, K4 item4, K5 item5, K6 item6)
-            : base(item1, item2, item3, item4, item5) { this.Item6 = item6; }
-
-        public K6 Item6
+            : base(item1, item2, item3, item4, item5)
         {
-            get; set;
+            this.Item6 = item6;
         }
+
+        public K6 Item6 { get; set; }
     }
 
     /// <summary>
@@ -174,6 +181,8 @@ namespace TAF.Utility
     /// </typeparam>
     /// <typeparam name="K7">
     /// </typeparam>
+    [DataContract]
+    [Serializable]
     public class KeyValue<K1, K2, K3, K4, K5, K6, K7> : KeyValue<K1, K2, K3, K4, K5, K6>
     {
         /// <summary>
@@ -183,11 +192,11 @@ namespace TAF.Utility
         }
 
         public KeyValue(K1 item1, K2 item2, K3 item3, K4 item4, K5 item5, K6 item6, K7 item7)
-            : base(item1, item2, item3, item4, item5, item6) { this.Item4 = item4; }
-
-        public K7 Item7
+            : base(item1, item2, item3, item4, item5, item6)
         {
-            get; set;
+            this.Item4 = item4;
         }
+
+        public K7 Item7 { get; set; }
     }
 }
